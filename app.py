7cdaf40286f50c30f8097469f94e2c1171385edb
Samwel Emmanuel <samakubi@gmail.com>
2025-06-08 11:54:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.write("Hello IK")
 st.title("Our Title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+
+
+prompt = st.chat_input("Ask Anything....")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
